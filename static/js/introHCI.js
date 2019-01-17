fa47aacaec8e48dc396c7167023b578d3be63841
atl043 <atl043@ucsd.edu>
2019-01-17 10:22:22 -0800

    final commit

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -2,10 +2,9 @@ $(document).ready(function() {
 	initializePage();
 })
 
-
 function initializePage() {
     $("a.thumbnail").click(projectClick);
-    
+
     $("#testjs").click(function(e) {
 
         $('.jumbotron h1').text("Javascript is connected");
